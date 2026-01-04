c361acc17d253bd9de6bd1bc9c29c0716ef1a92c
Saurabh88sr <saurabh888vns@gmail.com>
2026-01-04 08:33:09 +0530

    port envv

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://ruzzle-backend.onrender.com", {
+const URL = process.env.NODE_ENV === "production" 
+  ? "https://ruzzle-backend.onrender.com" 
+  : "http://localhost:5000";
+
+const socket = io(URL, {
   withCredentials: true,
   transports: ["polling", "websocket"],
 });
